035fc0c12d8123cd070e95ea07eea6f2f2a9ebeb
jamzdbest <102725554+jamzdbest@users.noreply.github.com>
2024-08-30 06:37:01 +0800

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Recipient email
+    $to = "jamelbelvestre@gmail.com";
+
+    // Email subject
+    $email_subject = "New message from: $name - $subject";
+
+    // Email body
+    $email_body = "Name: $name\n";
+    $email_body .= "Email: $email\n\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
